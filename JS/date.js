b5d9adca55a0cc0919e4d9fff1fcd3947976d284
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-08-19 19:54:18 +0200

    website added

---
+++ JS/date.js
@@ -0,0 +1,9 @@
+var date = new Date();
+
+var id = document.getElementById("date");
+
+const f = new Intl.DateTimeFormat("hu-hu", {
+    dataStyle: "full"
+})
+
+id.innerHTML = "" + date.toLocaleString();
