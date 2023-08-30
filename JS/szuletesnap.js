86b1c49b6ef0b676e26edeb7a5533c7e267a2372
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2023-08-30 19:11:35 +0200

    Bug fix

--- JS/szuletesnap.js
+++ JS/szuletesnap.js
@@ -3,8 +3,8 @@ var hónapja = "3";
 var napja = "3";
 
 var most_date = new Date();
-var most_hónap = date.getMonth() + 1;
-var most_nap = date.getDate();
+var most_hónap = most_date.getMonth() + 1;
+var most_nap = most_date.getDate();
 
 var ma = document.getElementById("ma");
 var id = document.getElementById("szuletesnap");
